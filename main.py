bb3b4ae2f204fb8a459e6752f37fdbf7b6cfe58b
Venkatp23 <jaganmohanreddypappula@gmail.com>
2026-01-20 13:01:55 -0500

    feat: implemented JWT auth, frontend login, and security scanners

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 from fastapi import FastAPI, Header, HTTPException
 
 app = FastAPI(title="Secure-SRE API")
@@ -13,3 +14,46 @@ def get_data(x_api_key: str = Header(None)):
     if x_api_key != "secret-key-123":
         raise HTTPException(status_code=403, detail="Unauthorized")
     return {"data": "This is protected info"}
+"""
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from auth import create_access_token, verify_password, hash_password
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Mock Database for Day 2
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "hashed_password": hash_password("secret123"),
+    }
+}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_login(request: Request):
+    # This serves the HTML file we just created
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def read_dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = fake_users_db.get(form_data.username)
+    if not user or not verify_password(form_data.password, user["hashed_password"]):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    access_token = create_access_token(data={"sub": user["username"]})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/secure-data")
+async def get_secure_data(token: str = Depends(oauth2_scheme)):
+    # This endpoint is now protected by a JWT gate!
+    return {"message": "SRE Reliability Tip: Always rotate your secrets!"}
